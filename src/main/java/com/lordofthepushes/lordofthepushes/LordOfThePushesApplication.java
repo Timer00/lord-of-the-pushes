928d04bb88f2752a0550a00993ab8097776e28f9
Vinicius Vieira <vinicius.marcondes@flieger.software>
2021-04-02 11:35:27 -0300

    push spring files

---
+++ src/main/java/com/lordofthepushes/lordofthepushes/LordOfThePushesApplication.java
@@ -0,0 +1,13 @@
+package com.lordofthepushes.lordofthepushes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LordOfThePushesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LordOfThePushesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lordofthepushes/lordofthepushes/LordOfThePushesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lordofthepushes.lordofthepushes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LordOfThePushesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
